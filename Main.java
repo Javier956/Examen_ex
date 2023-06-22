36f93751387a6da18636273586797eb05ef2ca82
Javier <tuemail@example.com>
2023-06-22 13:18:30 +0200

    Cambiado el mensaje en ramaB

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Alumnos de ramaA");
+        System.out.println("Hola Alumnos de ramaB");
         System.out.println("Examen Convocatoria Extraordinaria de Entornos de Desarrollo");
     }
 }
+
 
