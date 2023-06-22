7f35eb37f753cbd56755c4d437246d90e1917a09
Javier <tuemail@example.com>
2023-06-22 13:07:54 +0200

    Cambiado el mensaje en ramaA

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
-package main;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Alumnos");
+        System.out.println("Hola Alumnos de ramaA");
+        System.out.println("Examen Convocatoria Extraordinaria de Entornos de Desarrollo");
     }
 }
 
